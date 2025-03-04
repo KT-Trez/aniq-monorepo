05abe8f472c3dae146ef44bacdad552abc3d46fc
Bartłomiej Wąś <63346093+KT-Trez@users.noreply.github.com>
2025-03-05 00:57:37 +0100

    chore: prepare monorepo setup

---
+++ apps/management-api/lib/WebServer.ts
@@ -0,0 +1,34 @@
+import { Server } from 'node:http';
+import { noop } from '@aniq/utils/noop.js';
+import { Router } from './Router.ts';
+import { WebServerRequest } from './WebServerRequest.ts';
+import { WebServerResponse } from './WebServerResponse.ts';
+
+export class WebServer extends Router {
+  #httpServer: Server<typeof WebServerRequest, typeof WebServerResponse>;
+
+  constructor() {
+    super();
+    this.#httpServer = new Server(
+      {
+        IncomingMessage: WebServerRequest,
+        ServerResponse: WebServerResponse,
+      },
+      this.#requestHandler,
+    );
+  }
+
+  listen(port: number) {
+    this.#httpServer.listen(port);
+  }
+
+  #requestHandler(req: WebServerRequest, res: WebServerResponse) {
+    const nextPath = req.nextPath;
+
+    if (!nextPath) {
+      return res.error({ status: 404, message: 'Not Found' });
+    }
+
+    super.handleRequest(nextPath, req, res).then(noop);
+  }
+}
--- index.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-echo "<h1>Goodbye world 🙂🔫</h1>";
